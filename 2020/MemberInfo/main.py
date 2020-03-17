d96ad6f2f017d2d2aac37fc45a0d5a938e22c383
chenyulong <chenyulong08@outlook.com>
2020-03-17 23:42:50 +0800

    2020/3/17 update MemberInfo

---
+++ 2020/MemberInfo/main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import pandas as pd
+
+_CURR_PATH = os.path.dirname(os.path.abspath(__file__))
+_INFO_PATH = os.path.join(_CURR_PATH, "InfoFile")
+print(_INFO_PATH)
+
+
+def get_all_file_path(root_path):
+    file_paths = []
+    for file in os.listdir(root_path):
+        file = os.path.join(root_path, file)
+        if os.path.isfile(file):
+            file_paths.append(file)
+    return file_paths
+
+
+for file in get_all_file_path(_INFO_PATH):
+    re = pd.read_excel(file)
+    print(re[re['工号'] > 44619].head())
+    df = pd.DataFrame(re)
+    #print(df)
+    
+
--- 2020/test.py
+++ 2020/test.py
@@ -1,3 +1,28 @@
 import os
+import sys
+import pandas as pd
 
-print(os.path)
+_CURR_PATH = os.path.dirname(os.path.abspath(__file__))
+_INFO_PATH = os.path.join(_CURR_PATH, "MemberInfo/InfoFile")
+print(_INFO_PATH)
+
+
+def get_all_file_path(root_path):
+    file_paths = []
+    for file in os.listdir(root_path):
+        file = os.path.join(root_path, file)
+        if os.path.isfile(file):
+            file_paths.append(file)
+    return file_paths
+
+
+for file in get_all_file_path(_INFO_PATH):
+    re = pd.read_excel(file)
+    print(re[re['工号'] == 44619].head())
+    print(re[re['职级'].map(lambda x: x.endswith('A'))].head())
+    print(re[(re['工号'] == 44619) & re['职级'].map(lambda x: x.endswith('A'))]['姓名'].values)
+    df = pd.DataFrame(re)
+    #print(df)
+    
+
+
